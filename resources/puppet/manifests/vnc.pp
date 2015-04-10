c065c718657ccf15266c18b9e7fda965d644cf63
JungJungIn <call518@gmail.com>
2015-04-10 13:15:29 +0900

    Completed: Install OpenNebula Cluster & VNCServer

--- resources/puppet/manifests/vnc.pp
+++ resources/puppet/manifests/vnc.pp
@@ -106,7 +106,7 @@ exec { "Start vncserver Service":
     user     => "root",
     timeout  => "0",
     logoutput => true,
-    unless   => "lsof -ni:5900",
+    unless   => "lsof -ni:55900",
     require  => Exec["Add Service - vncserver"],
 }
 
