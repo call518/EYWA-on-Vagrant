905254bc5f204d3d6b4037e1d2f30d174f280177
JungJungIn <call518@gmail.com>
2015-04-17 17:01:01 +0900

    Completed: Install OpenNebula Cluster

--- resources/puppet/files/eywa-remotes/save_TEMPLATE.sh
+++ resources/puppet/files/eywa-remotes/save_TEMPLATE.sh
@@ -4,7 +4,8 @@ VM_ID=$1
 TEMPLATE=$2
 SAVE_DIR="/var/lib/one/vms/$VM_ID"
 
-echo $TEMPLATE > $SAVE_DIR/TEMPLATE
-
 #XPATH="/var/tmp/one/hooks/eywa/xpath.rb -b $TEMPLATE"
 #$XPATH > $SAVE_DIR/TEMPLATE.xpath
+
+echo $TEMPLATE > $SAVE_DIR/TEMPLATE.base64
+echo $TEMPLATE | base64 --decode > $SAVE_DIR/TEMPLATE
