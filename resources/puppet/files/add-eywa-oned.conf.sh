483eea53e25edc4af68fe480e235f8ddd258a962
JungJungIn <call518@gmail.com>
2015-05-20 01:33:14 +0900

    bugfix and update

--- resources/puppet/files/add-eywa-oned.conf.sh
+++ resources/puppet/files/add-eywa-oned.conf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+ONE_CONF="/etc/one/oned.conf"
+
 sed -i "/RESTRICTED_ATTR/ s/^/#/" /etc/one/oned.conf
 if ! grep -q "EYWA Config" ${ONE_CONF}; then
-	cat /home/vagrant/add-oned.conf >> ${ONE_CONF}
+	cat /home/vagrant/add-eywa-oned.conf >> ${ONE_CONF}
 fi
