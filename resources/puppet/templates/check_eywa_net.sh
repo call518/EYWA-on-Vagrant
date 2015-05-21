9f2a61a8577f5464b734cfdd8f7febfc8dd90bdb
JungJungIn <call518@gmail.com>
2015-05-21 19:46:49 +0900

    update check_eywa_net.sh

--- resources/puppet/templates/check_eywa_net.sh
+++ resources/puppet/templates/check_eywa_net.sh
@@ -4,7 +4,8 @@ EYWA_VID=`grep "one-[0-9]" $domain | sed -e 's/<name>//g' | sed -e 's/<\/name>//
 TMP_FILE=`mktemp`
 trap "rm -f $TMP_FILE" EXIT
 CONTEXT_FILE="/var/lib/one/vms/${EYWA_VID}/context.sh"
-scp 192.168.33.10:$CONTEXT_FILE $TMP_FILE
+#scp 192.168.33.10:$CONTEXT_FILE $TMP_FILE
+ssh 192.168.33.10 "cat $CONTEXT_FILE | grep 'IS_EYWA'" > $TMP_FILE
 
 source $TMP_FILE
 
